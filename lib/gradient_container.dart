160ffa3a94f6f88a5aa50c6e0193b2309463982c
Kurt Carvey Cadenas <kurtcarvey.m.cadenas@gmail.com>
2024-09-27 14:27:39 +0800

    Add DiceRoller widget and update GradientContainer
    - Add DiceRoller widget to handle rolling dice and display the result.
    - Update GradientContainer to use the DiceRoller widget instead of static text.

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,30 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:roll_dice/dice_roller.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
+
+  final Color color1;
+  final Color color2;
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 98)
-          ],
+          begin: startAlignment,
+          end: endAlignment,
+          colors: [color1, color2],
         ),
       ),
       child: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(
-            fontSize: 28,
-            color: Colors.white,
-          ),
-        ),
+        child: DiceRoller(),
       ),
     );
   }
 }
+
+// class GradientContainer extends StatelessWidget {
+//   const GradientContainer({
+//     super.key,
+//     required this.colors,
+//   });
+
+//   final List<Color> colors;
+
+//   @override
+//   Widget build(context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//           begin: startAlignment,
+//           end: endAlignment,
+//           colors: colors,
+//         ),
+//       ),
+//       child: const Center(
+//         child: StyledText('Hello World!'),
+//       ),
+//     );
+//   }
+// }
